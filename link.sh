f619b4cda73ca5c7c76986b6f97dbff9353186b0
ObinataOkdlab <obinata@mns.k.u-tokyo.ac.jp>
2020-03-11 17:39:19 +0900

    update: Update link.sh
    コメントアウトで設定が容易になるように変更

--- link.sh
+++ link.sh
@@ -2,14 +2,27 @@
 
 DOT_DIRECTORY="${HOME}/dotfiles"
 
+## === zsh ===
+# ln -snfv ${DOT_DIRECTORY}/.zshrc ${HOME}/.zshrc
+# ln -snfv ${DOT_DIRECTORY}/.zprofile ${HOME}/.zprofile
 
-for f in .??*
-do
-	[[ ${f} = ".git" ]] && continue
-	[[ ${f} = ".gitignore" ]] && continue
-	[[ ${f} = ".DS_Store" ]] && continue
-	ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
-done
+
+## === bash ===
+# ln -snfv ${DOT_DIRECTORY}/.bash_profile ${HOME}/.bash_profile
+# ln -snfv ${DOT_DIRECTORY}/.bashrc ${HOME}/.bashrc
+# ln -snfv ${DOT_DIRECTORY}/.inputrc ${HOME}/.inputrc
+
+
+## === git ===
+# ln -snfv ${DOT_DIRECTORY}/.git_commit_template ${HOME}/.git_commit_template
+# ln -snfv ${DOT_DIRECTORY}/.gitconfig ${HOME}/.gitconfig
+# ln -snfv ${DOT_DIRECTORY}/.gitignore_global ${HOME}/.gitignore_global
+
+
+## === others ===
+# ln -snfv ${DOT_DIRECTORY}/.ctags ${HOME}/.ctags
+# ln -snfv ${DOT_DIRECTORY}/.latexmkrc ${HOME}/.latexmkrc
+# ln -snfv ${DOT_DIRECTORY}/.vimrc ${HOME}/.vimrc
+
 
 echo $(tput setaf 2)Deploy dotfiles complete!. $(tput sgr0)
-
