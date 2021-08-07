6eafb20c260af0a6cede53f7af27f20fd84fa97d
Oohikata <koki.obi.321@gmail.com>
2021-08-07 20:42:57 +0900

    update:

--- zsh/users/macbook_pro_2018/setting.zsh
+++ zsh/users/macbook_pro_2018/setting.zsh
@@ -9,7 +9,7 @@ export PATH=$PATH:$HOME/.istioctl/bin
 
 # Python
 export PATH="$HOME/.poetry/bin:$PATH"
-source ~/Settings/python/default/bin/activate
+source ~/Settings/python/envs/default/bin/activate
 export PIPENV_IGNORE_VIRTUALENVS=1
 
 # misc
