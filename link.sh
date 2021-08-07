1741a2caea973d86c66c1cb260eb9ed4fd830084
Oohikata <koki.obi.321@gmail.com>
2021-08-07 21:48:40 +0900

    update: Update filetype detect settings

--- link.sh
+++ link.sh
@@ -24,6 +24,7 @@ DOT_DIRECTORY="${HOME}/dotfiles"
 # ln -snfv ${DOT_DIRECTORY}/tex/.latexmkrc ${HOME}/.latexmkrc
 # ln -snfv ${DOT_DIRECTORY}/vim/.vimrc ${HOME}/.vimrc
 # ln -snfv ${DOT_DIRECTORY}/vim/ftplugin ${HOME}/.vim/ftplugin
+# ln -snfv ${DOT_DIRECTORY}/vim/ftdetect ${HOME}/.vim/ftdetect
 # ln -snfv ${DOT_DIRECTORY}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
 # ln -snfv ${DOT_DIRECTORY}/karabiner/assets/complex_modifications/mapping.json ${HOME}/.config/karabiner/assets/complex_modifications/mapping.json
 
---
+++ vim/ftdetect/filetype.vim
@@ -0,0 +1,6 @@
+" === TeX ===
+autocmd BufRead,BufNewFile *.tex  set filetype=tex
+
+
+" === CSV ===
+autocmd BufRead,BufNewFile *.csv  set filetype=csv
