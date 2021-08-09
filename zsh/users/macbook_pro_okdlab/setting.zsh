360de474d2e58367933cb898b9faf42d0c954e07
Obinata(Research) <k.obinata.1238@gmail.com>
2021-08-09 19:11:30 +0900

    add: Add user settings

---
+++ zsh/users/macbook_pro_okdlab/setting.zsh
@@ -0,0 +1,11 @@
+# TeX
+export PATH="/Users/obi-research/Settings/tex/latexmk-script:$PATH"
+
+
+# Python
+export PATH="$HOME/.poetry/bin:$PATH"
+source ~/Settings/python-settings/normal/bin/activate
+export PIPENV_IGNORE_VIRTUALENVS=1
+
+# misc
+curl "wttr.in/Tokyo?lang=ja&format=v2"
