637847c3e7a75e6450b957bab3b1630e711e5d88
Obinata(Research) <k.obinata.1238@gmail.com>
2021-10-04 13:36:04 +0900

    update: Update user-dependent python settings

--- zsh/users/macbook_pro_okdlab/setting.zsh
+++ zsh/users/macbook_pro_okdlab/setting.zsh
@@ -4,7 +4,7 @@ export PATH="/Users/obi-research/Settings/tex/latexmk-script:$PATH"
 
 # Python
 export PATH="$HOME/.poetry/bin:$PATH"
-source ~/Settings/python-settings/normal/bin/activate
+source ~/Settings/python/envs/default/bin/activate
 export PIPENV_IGNORE_VIRTUALENVS=1
 
 # misc
