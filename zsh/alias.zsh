d01448e57fe3fde8fbc54b65f27235bac4965902
Koki Obinata <koki.obi.321@gmail.com>
2021-11-21 07:06:26 +0900

    remove: Remove unused alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -7,8 +7,6 @@ alias l='ls -FG'
 alias la='ls -aFG'
 alias ll='ls -lFG'
 
-alias so='source'
-
 ### how to use: u2s (-c) spam.csv > spam-cp.csv
 alias u2s='iconv -f UTF-8 -t cp932 ' # UTF-8 --> cp932(Microsoft Shift_JIS)
 alias s2u='iconv -f cp932 -t UTF-8 '
