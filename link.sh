cb231fd745e7461d2006464aa4b283eb1d544fee
Koki Obinata <koki.obi.321@gmail.com>
2021-12-19 00:13:45 +0900

    clean: Refactoring

--- link.sh
+++ link.sh
@@ -6,22 +6,21 @@ DOT_DIRECTORY="${HOME}/dotfiles"
 # ln -snfv ${DOT_DIRECTORY}/zsh/.zshrc ${HOME}/.zshrc
 # ln -snfv ${DOT_DIRECTORY}/zsh/.zprofile ${HOME}/.zprofile
 
-
-## === git ===
-## NOTE: Please set .gitconfig by yourself
-# ln -snfv ${DOT_DIRECTORY}/git/.git_commit_template ${HOME}/.git_commit_template
-# ln -snfv ${DOT_DIRECTORY}/git/.gitignore_global ${HOME}/.gitignore_global
-
-
 ## === vim ===
 # ln -snfv ${DOT_DIRECTORY}/vim/.vimrc ${HOME}/.vimrc
 # mkdir -p ${HOME}/.vim
 # ln -snfv ${DOT_DIRECTORY}/vim/ftplugin ${HOME}/.vim/ftplugin
 # ln -snfv ${DOT_DIRECTORY}/vim/ftdetect ${HOME}/.vim/ftdetect
 
+## === git ===
+## NOTE: Please set .gitconfig by yourself
+# ln -snfv ${DOT_DIRECTORY}/git/.git_commit_template ${HOME}/.git_commit_template
+# ln -snfv ${DOT_DIRECTORY}/git/.gitignore_global ${HOME}/.gitignore_global
 
-## === others ===
+## === TeX ===
 # ln -snfv ${DOT_DIRECTORY}/tex/.latexmkrc ${HOME}/.latexmkrc
+
+## === Karabiner ===
 # ln -snfv ${DOT_DIRECTORY}/karabiner/assets/complex_modifications/mapping.json ${HOME}/.config/karabiner/assets/complex_modifications/mapping.json
 
 
