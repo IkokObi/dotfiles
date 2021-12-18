7b9ba803af3f7bc85f5218f9820ba5a3bf3412cd
Koki Obinata <koki.obi.321@gmail.com>
2021-12-19 00:00:46 +0900

    remove: Remove unused settings

--- link.sh
+++ link.sh
@@ -7,12 +7,6 @@ DOT_DIRECTORY="${HOME}/dotfiles"
 # ln -snfv ${DOT_DIRECTORY}/zsh/.zprofile ${HOME}/.zprofile
 
 
-## === bash ===
-# ln -snfv ${DOT_DIRECTORY}/bash/.bash_profile ${HOME}/.bash_profile
-# ln -snfv ${DOT_DIRECTORY}/bash/.bashrc ${HOME}/.bashrc
-# ln -snfv ${DOT_DIRECTORY}/bash/.inputrc ${HOME}/.inputrc
-
-
 ## === git ===
 ## NOTE: Please set .gitconfig by yourself
 # ln -snfv ${DOT_DIRECTORY}/git/.git_commit_template ${HOME}/.git_commit_template
@@ -27,9 +21,7 @@ DOT_DIRECTORY="${HOME}/dotfiles"
 
 
 ## === others ===
-# ln -snfv ${DOT_DIRECTORY}/.ctags ${HOME}/.ctags
 # ln -snfv ${DOT_DIRECTORY}/tex/.latexmkrc ${HOME}/.latexmkrc
-# ln -snfv ${DOT_DIRECTORY}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
 # ln -snfv ${DOT_DIRECTORY}/karabiner/assets/complex_modifications/mapping.json ${HOME}/.config/karabiner/assets/complex_modifications/mapping.json
 
 
