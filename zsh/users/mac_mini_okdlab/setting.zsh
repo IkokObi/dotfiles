efbf332c7d23d19b8aeeb01e4dc61f534b91620b
Koki Obinata(mac-mini) <koki.obi.321@gmail.com>
2021-12-20 13:56:55 +0900

    add: Add user settings for mac-mini

---
+++ zsh/users/mac_mini_okdlab/setting.zsh
@@ -0,0 +1,10 @@
+# Python
+# export PATH="$HOME/.poetry/bin:$PATH"
+source ~/dotfiles/setups/python/envs/default/bin/activate
+export PIPENV_IGNORE_VIRTUALENVS=1
+
+# TeX
+export PATH="/Users/obi-macmini/dotfiles/setups/tex:$PATH"
+
+# misc
+curl "wttr.in/Tokyo?lang=ja&format=v2"
