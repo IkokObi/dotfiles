f45a3870b29a0dd44e67f42d8f1e63a900d670c1
Obinata(Research) <koki.obi.321@gmail.com>
2022-01-24 17:09:12 +0900

    add: Add gcloud zshrc settings

--- zsh/users/macbook_pro_okdlab/setting.zsh
+++ zsh/users/macbook_pro_okdlab/setting.zsh
@@ -1,3 +1,10 @@
+# gcloud
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/obi-research/setups/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/obi-research/setups/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/obi-research/setups/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/obi-research/setups/google-cloud-sdk/completion.zsh.inc'; fi
+
 # TeX
 export PATH="/Users/obi-research/dotfiles/setups/tex:$PATH"
 
