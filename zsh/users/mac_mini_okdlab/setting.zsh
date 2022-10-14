57318d56a07fe20037d97e4914193c7d96aeaf78
Koki Obinata(mac-mini) <koki.obi.321@gmail.com>
2022-10-14 13:56:06 +0900

    update:

--- zsh/users/mac_mini_okdlab/setting.zsh
+++ zsh/users/mac_mini_okdlab/setting.zsh
@@ -1,3 +1,10 @@
+# Python環境は切り替えることが多いので、先に以下のコマンドを実行する
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/obi-macmini/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/obi-macmini/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/obi-macmini/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/obi-macmini/google-cloud-sdk/completion.zsh.inc'; fi
+
 # Python
 export PATH="$HOME/.local/bin:$PATH"
 source ~/dotfiles/setups/python/envs/default/bin/activate
