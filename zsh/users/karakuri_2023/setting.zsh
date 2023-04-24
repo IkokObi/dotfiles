1a3cea26d7471310e22a8708106ec75e9e13b77a
Koki Obinata <koki.obi.321@gmail.com>
2023-04-24 19:37:25 +0900

    feat: Add gcloud setting

--- zsh/users/karakuri_2023/setting.zsh
+++ zsh/users/karakuri_2023/setting.zsh
@@ -1,3 +1,9 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/completion.zsh.inc'; fi
+
 # Python
 # export PATH="$HOME/.poetry/bin:$PATH"
 source ~/dotfiles/setups/python/envs/default/bin/activate
