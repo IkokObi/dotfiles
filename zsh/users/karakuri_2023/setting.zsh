c1872bca8797779d5b9520ec809a2275a90b65c5
Koki Obinata <koki.obi.321@gmail.com>
2023-06-08 16:17:00 +0900

    fix: Fix node and python setting order
    pythonのdefault envがactivateされPath設定される
    -> fnmのPath設定がされる
    -> 別のvenvが再度設定される
    の順序だと、最初のdefault env以降に設定したPathが消える模様

--- zsh/users/karakuri_2023/setting.zsh
+++ zsh/users/karakuri_2023/setting.zsh
@@ -4,6 +4,9 @@ if [ -f '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/path.zsh.inc' 
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/completion.zsh.inc'; fi
 
+# node
+eval "$(fnm env --use-on-cd)"
+
 # Python
 # export PATH="$HOME/.poetry/bin:$PATH"
 source ~/dotfiles/setups/python/envs/default/bin/activate
@@ -11,9 +14,6 @@ export PIPENV_IGNORE_VIRTUALENVS=1
 
 # ログインシェル時にvenvチェックを実行する
 auto_venv_activate
-
-# node
-eval "$(fnm env --use-on-cd)"
 
 # misc
 curl "wttr.in/Tokyo?lang=ja&format=v2"
