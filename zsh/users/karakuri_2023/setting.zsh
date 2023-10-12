84a84584fd8f4b40bdf805391bc7fbd5519f4aad
Koki Obinata <koki.obi.321@gmail.com>
2023-10-12 19:27:24 +0900

    feat: Update gcloud config

--- zsh/users/karakuri_2023/setting.zsh
+++ zsh/users/karakuri_2023/setting.zsh
@@ -1,8 +1,8 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f '/Users/k.obinata/dotfiles/setups/private-tools/gcloud/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/k.obinata/dotfiles/setups/private-tools/gcloud/google-cloud-sdk/path.zsh.inc'; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/k.obinata/dotfiles/setups/gcloud/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f '/Users/k.obinata/dotfiles/setups/private-tools/gcloud/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/k.obinata/dotfiles/setups/private-tools/gcloud/google-cloud-sdk/completion.zsh.inc'; fi
 
 # node
 eval "$(fnm env --use-on-cd)"
