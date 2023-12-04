0bc0d7ea84960e5c257b7d1a9de93d6c811caf91
Koki Obinata <koki.obi.321@gmail.com>
2023-12-05 03:03:43 +0900

    feat: Add fnm setting

--- zsh/users/karakuri_2023_temp/setting.zsh
+++ zsh/users/karakuri_2023_temp/setting.zsh
@@ -3,5 +3,8 @@ export PATH="$HOME/.local/bin:$PATH"
 source ~/dotfiles/setups/python/envs/default/bin/activate
 # export PIPENV_IGNORE_VIRTUALENVS=1
 
-# ログインシェル時にvenvチェックを実行する
+# node
+eval "$(fnm env --use-on-cd)"
+
+# ログインシェル時にvenvチェックを実行する（このactivateは最後に実行する）
 auto_venv_activate
