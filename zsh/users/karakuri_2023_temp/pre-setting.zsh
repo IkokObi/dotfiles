bac475846535a7290c9c3461c97473639f83129d
Koki Obinata <koki.obi.321@gmail.com>
2023-12-05 00:51:18 +0900

    feat: Add karakuri_2023_temp user setting

---
+++ zsh/users/karakuri_2023_temp/pre-setting.zsh
@@ -0,0 +1,5 @@
+# Homebrew setting
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# fasd
+export PATH="$HOME/dotfiles/setups/fasd:$PATH"
---
+++ zsh/users/karakuri_2023_temp/setting.zsh
@@ -0,0 +1,7 @@
+# Python
+export PATH="$HOME/.local/bin:$PATH"
+source ~/dotfiles/setups/python/envs/default/bin/activate
+# export PIPENV_IGNORE_VIRTUALENVS=1
+
+# ログインシェル時にvenvチェックを実行する
+auto_venv_activate
