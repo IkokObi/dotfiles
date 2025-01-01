9d7535a8e41049ec2c6639f7960facaaf29deaad
Koki Obinata <koki.obi.321@gmail.com>
2025-01-01 12:06:31 +0900

    refactor: Refactor git/init.sh

--- git/init.sh
+++ git/init.sh
@@ -6,6 +6,9 @@ source ${DOTFILES}/config/config.sh # Set DOTFILES_USER variables
 ln -snfv ${DOTFILES}/git/.git_commit_template ${HOME}/.git_commit_template
 ln -snfv ${DOTFILES}/git/.gitignore_global ${HOME}/.gitignore_global
 
-if [ -f ${DOTFILES}/users/${DOTFILES_USER}/.gitconfig.user ]; then
-    ln -snfv ${DOTFILES}/users/${DOTFILES_USER}/.gitconfig.user ${HOME}/.gitconfig.user
+GITCONFIG_USER=${DOTFILES}/users/${DOTFILES_USER}/.gitconfig.user
+if [ -f ${GITCONFIG_USER} ]; then
+    ln -snfv ${GITCONFIG_USER} ${HOME}/.gitconfig.user
+else
+    echo "Warning: ${GITCONFIG_USER} does not exist"
 fi
