1f29700453d257b02332501ad41bedbc35c0e52c
Koki Obinata <koki.obi.321@gmail.com>
2025-01-01 12:17:38 +0900

    feat: Use synbolic link to .gitconfig

--- git/init.sh
+++ git/init.sh
@@ -5,6 +5,7 @@ source ${DOTFILES}/config/config.sh # Set DOTFILES_USER variables
 
 ln -snfv ${DOTFILES}/git/.git_commit_template ${HOME}/.git_commit_template
 ln -snfv ${DOTFILES}/git/.gitignore_global ${HOME}/.gitignore_global
+ln -snfv ${DOTFILES}/git/.gitconfig ${HOME}/.gitconfig
 
 GITCONFIG_USER=${DOTFILES}/users/${DOTFILES_USER}/.gitconfig.user
 if [ -f ${GITCONFIG_USER} ]; then
