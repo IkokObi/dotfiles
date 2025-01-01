1e77b1abe87fe1e3db1ba10cf73c41ed56c55901
Koki Obinata <koki.obi.321@gmail.com>
2025-01-01 16:50:23 +0900

    feat: Update vim/init.sh

--- vim/init.sh
+++ vim/init.sh
@@ -2,8 +2,9 @@
 
 DOTFILES=${HOME}/dotfiles
 
-mkdir -p ${HOME}/.vim/after
 mkdir -p ${HOME}/.vimundo
+mkdir -p ${HOME}/.vim/after
+mkdir -p ${HOME}/.vim/spell
 ln -snfv ${DOTFILES}/vim/.vimrc ${HOME}/.vimrc
 ln -snfv ${DOTFILES}/vim/after/ftplugin ${HOME}/.vim/after/ftplugin
 ln -snfv ${DOTFILES}/vim/ftdetect ${HOME}/.vim/ftdetect
