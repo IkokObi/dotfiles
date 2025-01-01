7b2dbc1612ce287d9cf45c48e3cc10534efb043c
Koki Obinata <koki.obi.321@gmail.com>
2025-01-01 12:35:07 +0900

    feat: Add .vimundo directory creation

--- vim/init.sh
+++ vim/init.sh
@@ -3,6 +3,7 @@
 DOTFILES=${HOME}/dotfiles
 
 mkdir -p ${HOME}/.vim/after
+mkdir -p ${HOME}/.vimundo
 ln -snfv ${DOTFILES}/vim/.vimrc ${HOME}/.vimrc
 ln -snfv ${DOTFILES}/vim/after/ftplugin ${HOME}/.vim/after/ftplugin
 ln -snfv ${DOTFILES}/vim/ftdetect ${HOME}/.vim/ftdetect
